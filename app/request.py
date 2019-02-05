a0c62f2a538d847ad05635c8597814f37cb4fb2e
Fahari <karonjekevin67@gmail.com>
2019-02-05 11:39:30 +0300

    get data from API.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,12 @@
 from app import app
 
+import urllib.request,json
+from .models import source
+
+Source = source.Source
+
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+baseSource_url = app.config["SOURCE_API_BASE_URL"]
