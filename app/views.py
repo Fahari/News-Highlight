cb9d274169efadc624c151db432a73f06ce6b650
Fahari <karonjekevin67@gmail.com>
2019-02-05 10:44:20 +0300

    create the source page view function

---
+++ app/views.py
@@ -0,0 +1,12 @@
+
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def source():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('source.html')
