f7bfe2ca4f98cea26270548ccf5005cf9df955a6
Fahari <karonjekevin67@gmail.com>
2019-02-05 12:01:49 +0300

    process the results and source movie objects

--- app/request.py
+++ app/request.py
@@ -10,3 +10,45 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the movie base url
 baseSource_url = app.config["SOURCE_API_BASE_URL"]
+
+def get_source(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = baseSource_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+
+        source_results = None
+
+        if get_source_response['results']:
+            source_results_list = get_source_response['results']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
+def process_results(source_list):
+    '''
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+
+        if category:
+            source_object = Source(name,description,url,category)
+            source_results.append(source_object)
+
+    return source_results
