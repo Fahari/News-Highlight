d7b10546d0def4a60d0befbe766c3dc37521ecbe
Fahari <karonjekevin67@gmail.com>
2019-02-05 11:35:12 +0300

    pass the test

---
+++ app/models/source.py
@@ -0,0 +1,10 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,name,description,url,category):
+        self.name =name
+        self.description = description
+        self.url = url
+        self.category = category
