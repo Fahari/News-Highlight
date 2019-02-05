0ac09f587cd86ae8bc97822e4ccf970daa4a2b82
Fahari <karonjekevin67@gmail.com>
2019-02-05 10:55:59 +0300

    add some logic control flows

--- app/views.py
+++ app/views.py
@@ -9,4 +9,5 @@ def source():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('source.html')
+    title = 'Home - Welcome to News Highlight'
+    return render_template('source.html', title = title)
