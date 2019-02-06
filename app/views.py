415913133c7cc857f92c18a4049abb3138ceacc2
Fahari <karonjekevin67@gmail.com>
2019-02-06 10:40:18 +0300

    Add More Categories to view.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 
 from flask import render_template
 from app import app
+from .request import get_source
 
 # Views
 @app.route('/')
@@ -10,4 +11,12 @@ def source():
     View root page function that returns the index page and its data
     '''
     title = 'Home - Welcome to News Highlight'
-    return render_template('source.html', title = title)
+    general_news = get_source('general')
+    business_news = get_source('business')
+    entertainment_news = get_source('entertainment')
+    health_news = get_source('health')
+    science_news = get_source('science')
+    sports_news = get_source('sports')
+    technology_news = get_source('technology')
+    # print(general_news)
+    return render_template('source.html', title = title,general = general_news,business = business_news,entertainment = entertainment_news,health = health_news,science = science_news,sports = sports_news,technology = technology_news)
