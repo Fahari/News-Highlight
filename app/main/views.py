0c004322fe97f81f6722e23d2324907ac32a784c
Fahari <karonjekevin67@gmail.com>
2019-02-07 12:07:56 +0300

    update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -10,17 +10,21 @@ def source():
     '''
     View root page function that returns the source page and its data
     '''
-    #Getting general news
+
     general_news = get_source('general')
     print(general_news)
-    #getting technology news
+
     technology_news = get_source('technology')
-    #getting sport_news
+
     sport_news = get_source('sports')
-    #getting business news
+
     business_news = get_source('business')
-    #getting science news
+
     science_news = get_source('science')
 
+    entertainment_news = get_source('entertainment')
+
+    health_news = get_source('health')
+
     title = 'Home -Let us get to the briefs'
-    return render_template('source.html', title = title,general=general_news,technology=technology_news,sports=sport_news,business=business_news,science=science_news)
+    return render_template('source.html', title = title,general=general_news,technology=technology_news,sports=sport_news,business=business_news,science=science_news,entertainment_news=entertainment,health_news=health)
